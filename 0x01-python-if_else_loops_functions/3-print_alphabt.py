9797496366e7c9137304fc8981023d9d08f1669f
stacie-kipruto <staciekipruto@gmail.com>
2022-06-01 04:18:52 -0700

    print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!usr/bin/python3
+
 for i in range(97, 123):
-    if (i != 101):
-        if(i != 113):
-            print("{:c}".format(i), end="")
+    if i == 113 or i == 101:
+        continue
+    print("{:c}".format(i), end="")
