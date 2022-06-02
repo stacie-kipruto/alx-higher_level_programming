c93a646b4978a5d6b8b01be4117cdeeb5dcabc17
stacie-kipruto <staciekipruto@gmail.com>
2022-06-01 23:37:55 -0700

    combination of brains

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 for i in range (0,10):
     for j in range (1,10):
         if i >= j:
@@ -6,4 +6,4 @@ for i in range (0,10):
         if i ==8 and j ==9:
             print("{:d}{:d}".format(i,j))
         else:
-            print("{:d}{:d}".format(i, j), end =", "
+            print("{:d}{:d}".format(i, j), end =", ")
