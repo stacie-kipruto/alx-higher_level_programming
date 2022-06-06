e7ecb6cbd375c2a89304b29647963d5109869a54
stacie-kipruto <staciekipruto@gmail.com>
2022-06-06 08:29:00 -0700

    correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     s = " "
-    for i in my_string:
-        if i == "c" or i == "C":
-        s += i
+    for char in my_string:
+        if char != "c" and char != "C" :
+         s += char
     return s
