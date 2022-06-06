d4f0f49990e3d5fd8d9ef5f8d316de6f814746a1
stacie-kipruto <staciekipruto@gmail.com>
2022-06-06 08:22:56 -0700

    Can you C me now

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     s = " "
-    for i in my_string:
-        if i == "c" or i == "C":
+    for char in my_string:
+        if char != "c" or char != "C":
             continue
-        s += i
+        s += char
     return s
