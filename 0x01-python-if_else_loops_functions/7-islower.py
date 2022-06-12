93d32fb27d7be707e01be827d98f1d198ec4518f
stacie-kipruto <staciekipruto@gmail.com>
2022-06-12 14:27:59 -0700

    islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def islower(c):
+    if c != "":
+        if c.islower():
+            return True
+        else:
+            return False
+    else:
+        return "Traceback (most recent call last): "
