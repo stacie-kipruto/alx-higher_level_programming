9d7a85372afc430e223a6e4053b7608e8ba230c9
stacie-kipruto <staciekipruto@gmail.com>
2022-06-14 07:20:16 -0700

    lists of matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
             if i != row[-1]:
-                print("{:d}".format(i), end="")
+                print("{:d}".format(i), end=" ")
             else:
-                print("{:d}".format(i), end="")
+                print("{:d}".format(i), end=" ")
         print()
