20472ff49f8cc3be1ed5da135be0416e7be4e75a
stacie-kipruto <staciekipruto@gmail.com>
2022-07-06 08:48:44 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+5-base_geometry module
+"""
+
+
+class BaseGeometry:
+    """
+    public method "area"
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
