61892f7748c0b7463b25c13b383a85ee0d0f18e4
stacie-kipruto <staciekipruto@gmail.com>
2022-07-12 06:36:10 -0700

    access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+module documentation
+"""
+
+
+class Square:
+    """
+    Square documentation
+    """
+    def __init__(self, size=0):
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, x):
+        if type(x) == int:
+            if x >= 0:
+                self.__size = x
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
