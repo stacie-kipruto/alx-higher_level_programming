139ef4b7f7d87dc732cd243b342a0379dd8325bb
stacie-kipruto <staciekipruto@gmail.com>
2022-08-11 12:01:45 -0700

    full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- script to creat table 'second_table' in the hbtn_0c_0 database and ad multiples of rows
+
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES(1, 'John', 10);
+INSERT INTO second_table VALUES(2, 'Alex', 3);
+INSERT INTO second_table VALUES(3, 'Bob', 14);
+INSERT INTO second_table VALUES(4, 'George', 8);
