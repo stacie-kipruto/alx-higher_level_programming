0691bc8984784169ace29b0399bda9c4a5e3c8d1
stacie-kipruto <staciekipruto@gmail.com>
2022-08-11 12:37:17 -0700

    Temperatures #0

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- display avg temperature (Fahrenheit) by cityordered by temperature (descending)
+SELECT `city`, AVG(`value`) AS avg_temp FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
