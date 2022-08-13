a3733c91f2755c397d7d63a77805995b9fd4b7b9
stacie-kipruto <staciekipruto@gmail.com>
2022-08-13 09:59:57 -0700

    Genre ID

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- list all shows contained in `hbtn_0d_tvshows`
+-- each record should display `tv_shows.title` `tv_show_genres.genre_id`
+-- results must be sorted in ascending order by `tv_shows.title` and `tv_show_genres.genre_id`
+-- if a show does not have a genre, display NULL
+-- you can use only one SELECT statement
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+FROM `tv_shows`
+LEFT JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
