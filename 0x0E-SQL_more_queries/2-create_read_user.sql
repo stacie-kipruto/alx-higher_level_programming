5e0837595a7bc855f49ceb7ed62e8388a94cbb26
stacie-kipruto <staciekipruto@gmail.com>
2022-08-13 09:23:50 -0700

    Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- creates `hbtn_0d_2` database and `user_0d_2` user with only SELECT privilege and password `user_0d_2_pwd`
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`;
