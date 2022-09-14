d7b7254bd348609bf2463406441f2d0aeba12ab0
stacie-kipruto <staciekipruto@gmail.com>
2022-09-14 04:21:38 -0700

    correction

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""
-2-my_filter_states: select statess matching argument
-"""
-
+""" Take in an argument and display all values
+    in the states table of hbtn_0e_0_usa
+    where name matches the argument """
 
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-      host="localhost",
-      user=sys.argv[1],
-      passwd=sys.argv[2],
-      database=sys.argv[3],
-      port=3306)
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE \
-        '{}' ORDER BY id".format(sys.argv[4]))
-    results = cur.fetchall()
-    for result in results:
-        print(result)
-    cur.close()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
